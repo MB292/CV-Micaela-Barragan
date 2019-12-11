f926cf09ca4dd8f6502c97fb69530de602d557d5
MB292 <barraganmica@gmail.com>
2019-12-11 17:43:41 -0300

    trabajo final

--- index.js
+++ index.js
@@ -1,45 +1,18 @@
-var productos={
-    "chexa":{ "nombre":"Chexa",
-              "direccion":"Av. Fuerza Aérea 3700",
-              "telefono":03514789200 },
-    "turin":{ "nombre":"Turin",
-              "direccion":"Av. Fuerza Aérea 3808",
-              "telefono":03514879300 },
-    "hiperplatinum":{ "nombre":"HiperPlatinum",
-                      "direccion":"Av. Fuerza Aérea 3750",
-                      "telefono":03514298755 },
-    "hiperauto":{ "nombre":"HiperAuto",
-                  "direccion":"Av. Fuerza Aérea 3780",
-                  "telefono":03514290125 }
-}
-
-$(".individual").click(function(){
-    var id= $(this).attr ("productID")
-    var producto= productos[id]
-    
-$(".nombre").text(producto.nombre)
-$(".direccion").text(producto.direccion)
-$(".telefono").text(producto.telefono)
-$(".carta").addClass("visible")
-
-});
-
 
 $(".boton").click (function(){
-  $(".ventanaoculta").addClass("ventanavisible")
- });
+  $(".ventanaoculta").slideToggle("slow")
  
+});
 
+$(document).ready(function(){
+  $('.autoplay').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+});
 
-var db= firebase.database()
-db.ref("Fiat/Furgón").on("value",function(datalist){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
-var lista=datalist.val()
-var toshow=""
-for(var i in lista){
-toshow+="<div class='col-md-4'><p>"+lista[i].Versión+"</p><p>"+lista[i].Patente+"</p><p>"+lista[i].Combustible+"</p></div>"
-
-}
-$(".tocomplete").html(toshow)
-})
-
-
+/* No logré hacer funcionar el formulario, si bien aparece en todas las páginas,
+ sólo en Contactanos.js está el .js del formulario escrito, ya que sino al escribirlo 
+ en las otras páginas dejaban de funcionar todas las funciones de .js que tenían.*/
